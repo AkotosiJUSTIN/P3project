4f21a36a2d0a8487b3d4009835b50e198f6ee614
AkotosiJUSTIN <127915260+AkotosiJUSTIN@users.noreply.github.com>
2023-11-21 20:42:32 +0800

    Add files via upload

--- 1st sem P3/Final_Project.py
+++ 1st sem P3/Final_Project.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog
+from tkinter import *
+import sys
 import random
-import time
 
 # Dictionary of operator type
 operator_type = {"add": "+", "subtract": "-", "multiply": "*", "divide": "/"}
@@ -9,11 +10,17 @@ operator_type = {"add": "+", "subtract": "-", "multiply": "*", "divide": "/"}
 # GUI initialization
 root = tk.Tk()
 root.title("Math Mastermind")
-root.geometry("300x200")
+root.geometry("600x500")
+root['bg']='#5ba56e'
 
+#title
+title = tk.Label(root, text="Math Mastermind",fg='black', height='3', font=("Arial",40))
+title['bg']='#5ba56e'
+title.pack()
 
 #text on top of the drawer
-difficulty_label = tk.Label(root, text="Choose a difficulty level:")
+difficulty_label = tk.Label(root, text="Choose a difficulty level:", fg='black', font=("Arial",20))
+difficulty_label['bg']='#5ba56e'
 difficulty_label.pack()
 
 #drawer of difficulties
@@ -21,9 +28,11 @@ difficulty_var = tk.StringVar()
 difficulty_var.set("easy")
 difficulty_menu = tk.OptionMenu(root, difficulty_var, "easy", "moderate", "hard")
 difficulty_menu.pack()
+difficulty_menu.config(font=("Arial",20), border=0, indicator=0, bg="#36c2f3", activebackground="#5ba56e")
 
 #text on top of the drawer
-equation_label = tk.Label(root, text="Choose an equation type:")
+equation_label = tk.Label(root, text="Choose an equation type:", fg='black', font=("Arial",20))
+equation_label['bg']='#5ba56e'
 equation_label.pack()
 
 #drawer of equations
@@ -31,14 +40,17 @@ equation_var = tk.StringVar()
 equation_var.set("add")
 equation_menu = tk.OptionMenu(root, equation_var, "add", "subtract", "multiply", "divide")
 equation_menu.pack()
+equation_menu.config(font=("Arial",20), border=0, indicator=0, bg="#36c2f3", activebackground="#5ba56e")
 
 #text on top of the drawer
-num_problems_label = tk.Label(root, text="Enter the number of problems:")
+num_problems_label = tk.Label(root, text="Enter the number of problems:",  fg='black', font=("Arial",20))
+num_problems_label['bg']='#5ba56e'
 num_problems_label.pack()
 
 #box that user can put their answer about how many problems they want to answer
 num_problems_entry = tk.Entry(root)
 num_problems_entry.pack()
+num_problems_entry.config(font=("Arial",20), border=0)
 
 # Button to start the game
 def start_game():
@@ -59,7 +71,9 @@ def start_game():
     play_game(difficulty, equation_type, score)
 
 play_button = tk.Button(root, text="Play Game", command=start_game)
+play_button['bg']='#5ba56e'
 play_button.pack()
+play_button.config(font=("Arial",20), border=0, bg="#36c2f3")
 
 def play_game(difficulty, equation_type, score):
     num_problems = score
@@ -68,28 +82,25 @@ def play_game(difficulty, equation_type, score):
     while num_problems > 0:
         equation = make_equation(difficulty, equation_type)
 
-        #limit answers and timer of 10 seconds fo hard difficulty
+        #limit answers 
         if difficulty == "hard":
-            answer_chance = 3
-            timer = 10
-            time_start = time.time()
+            answer_chance = 1
         elif difficulty == "moderate":
             answer_chance = 3
         else:
-            answer_chance = 100
+            answer_chance = 10
 
-        while answer_chance > 0 and (difficulty in ["easy", "moderate"] or (difficulty == "hard" and timer > 0)):
-            if difficulty == "hard":
-                #prints the value of timer (it's 10) and gets answer
-                answer = simpledialog.askstring("Input", f"What is the answer to: {equation}?-----Time remaining: {timer:.1f} seconds")
-            else:
-                #gets answer(easy, moderate difficulties)
-                answer = simpledialog.askstring("Input", f"What is the answer to: {equation}?")
-
-            #when cancel is clicked, it goes to the next question
-                if answer is None:
-                    break
-                        
+        while answer_chance > 0:
+            #gets answer(easy, moderate difficulties)
+            answer = simpledialog.askstring("Input", f"What is the answer to: {equation}? \n Chances remaining: {answer_chance}")
+            
+            #if cancel button is used, the game will break
+            if answer is None:
+                messagebox.showinfo("Math Mastermind", "!!!Game Over!!! \n Game canceled by user.")
+                root.destroy()
+                sys.exit(0)
+                
+            
             try:
                 #evaluate/solve the equation/math problem
                 result = eval(equation)
@@ -110,16 +121,6 @@ def play_game(difficulty, equation_type, score):
             #if the user didn't enter a number
             except ValueError:
                 messagebox.showerror("Math Mastermind", "!!!Error!!! \n Invalid input. Please enter a numeric value.")
-
-                if difficulty == "hard":
-                    #calculate the remaining time since the 10 seconds starts
-                    time_remaining = timer - (time.time() - time_start)
-                    #checks the time
-                    if time_remaining <= 0:
-                        messagebox.showinfo("Math Mastermind", "Out of time!")
-                        break
-                    #updates the time remaining variable
-                    timer = time_remaining
             #decrements the math problems
         num_problems -= 1
 
@@ -142,14 +143,14 @@ def make_equation(difficulty, equation_type):
     operator = operator_type[equation_type]
 
     if difficulty == "easy":
-        num1 = random.randint(0, 10)
-        num2 = random.randint(0, 10)
+        num1 = random.randint(0, 100)
+        num2 = random.randint(0, 100)
     elif difficulty == "moderate":
-        num1 = random.randint(11, 100)
-        num2 = random.randint(11, 100)
-    elif difficulty == "hard":
         num1 = random.randint(101, 1000)
         num2 = random.randint(101, 1000)
+    elif difficulty == "hard":
+        num1 = random.randint(1001, 5000)
+        num2 = random.randint(1001, 5000)
     else:
         raise ValueError("Invalid Difficulty Level.")
 
